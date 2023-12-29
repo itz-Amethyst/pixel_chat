ed37cc59ae67f34b7af3b3f90ad800090c52cc3d
itz-amethyst <pransermi@gmail.com>
2023-12-29 21:05:39 +0330

    feat: creating basic structures

--- PixelChat/settings.py
+++ PixelChat/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #! External
+    "account",
+    "server"
 ]
 
 MIDDLEWARE = [
@@ -113,3 +116,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account"
