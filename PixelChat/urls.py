52efa7a4677104aeb9d3d4b5dfa5638c126e6339
itz-amethyst <pransermi@gmail.com>
2023-12-30 00:05:01 +0330

    feat: list api server

--- PixelChat/urls.py
+++ PixelChat/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+import server.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/docs/schema', SpectacularAPIView.as_view(), name="schema"),
-    path('api/docs/swagger', SpectacularSwaggerView.as_view()),
+    path('api/docs/', SpectacularSwaggerView.as_view()),
+
+    path('api/server/', include(server.urls.router.urls))
 ]
