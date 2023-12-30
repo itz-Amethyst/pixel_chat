c1038b5056f6580bcd25e2ffd958cf16c06ee30c
itz-amethyst <pransermi@gmail.com>
2023-12-30 21:45:11 +0330

    feat: added banner for server

--- server/models.py
+++ server/models.py
@@ -7,6 +7,14 @@ from django.dispatch import receiver
 from rest_framework.generics import get_object_or_404
 
 
+#Todo: Refactor this
+def server_icon_upload_path(instance, filename):
+    return f"server/server_icons/{instance.id} - {instance.name}/{filename}"
+
+def server_banner_upload_path(instance, filename):
+    return f"server/server_banners/{instance.id} - {instance.name}/{filename}"
+
+
 def category_icon_upload_path(instance, filename):
     # return f"category/{instance.id}/category_icons/{filename}"
 
@@ -65,12 +73,31 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name = 'channel_owner')
     topic = models.CharField(max_length = 100)
     server = models.ForeignKey(Server, on_delete = models.CASCADE, related_name = 'channel_server')
+    banner = models.ImageField(upload_to = server_banner_upload_path, null = False, blank = False)
+    icon = models.ImageField(upload_to = server_icon_upload_path, null = False, blank = False)
 
-    def save(
-        self, force_insert=False, force_update=False, using=None, update_fields=None
-    ):
+    def save(self, *arg, **kwargs):
         self.name = self.name.lower()
-        super(Channel, self).save()
+        if self.id:
+            existing = get_object_or_404(Channel, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save = False)
+        super(Channel, self).save(*arg, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Server")
+    def server_delete_files(sender, instance, **kwargs):
+        for field in ("icon", "banner"):
+            file_field = instance._meta.get_field(field)
+            if isinstance(file_field, (models.ImageField, models.FileField)):
+                file = getattr(instance, field, None)
+                if file and file.storage.exists(file.name):
+                    # file.storage.delete(file.name)
+
+                    # Remove entire folder
+                    folder_path = os.path.join(settings.MEDIA_ROOT, os.path.dirname(file.name))
+                    shutil.rmtree(folder_path, ignore_errors=True)
 
     def __str__(self):
         return self.name
