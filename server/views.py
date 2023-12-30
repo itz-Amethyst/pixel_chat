07004503cf18aafc4264df0b8e5e85f623b267a9
itz-amethyst <pransermi@gmail.com>
2023-12-30 17:16:00 +0330

    feat: current_user & take

--- server/views.py
+++ server/views.py
@@ -10,9 +10,18 @@ class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
     def list(self, request):
         category = request.query_params.get("category")
+        only_logged_user = request.query_params.get("current_user") == "true"
+        count = request.query_params.get("take")
 
         if category:
             self.queryset = self.queryset.filter(category__name = category)
+
+        if only_logged_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member = user_id)
+
+        if count:
+            self.queryset = self.queryset.filter[:int(count)]
 
         serializer = ServerSerializer(self.queryset, many = True)
         return Response(serializer.data, status = status.HTTP_200_OK)
