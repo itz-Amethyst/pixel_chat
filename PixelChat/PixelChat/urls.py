c6d4aa0202318114b1c9ea0af6d78faf9c5cdb45
itz-amethyst <pransermi@gmail.com>
2024-01-02 21:01:14 +0330

    feat: jwt token in url

--- PixelChat/PixelChat/urls.py
+++ PixelChat/PixelChat/urls.py
@@ -3,6 +3,7 @@ from django.urls import path , include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView , TokenRefreshView
 
 import server.urls
 
@@ -11,7 +12,10 @@ urlpatterns = [
     path('api/docs/schema', SpectacularAPIView.as_view(), name="schema"),
     path('api/docs/', SpectacularSwaggerView.as_view()),
 
-    path('api/server/', include(server.urls.router.urls))
+    path('api/server/', include(server.urls.router.urls)),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
