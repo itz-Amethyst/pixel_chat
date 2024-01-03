6b47391ab816240835c4a32f9d65ba3750f1f26e
itz-amethyst <pransermi@gmail.com>
2024-01-03 21:15:07 +0330

    feat: log out api

--- PixelChat/account/views.py
+++ PixelChat/account/views.py
@@ -1,10 +1,22 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Account
 from .schemas import user_list_docs
 from .serializers import AccountSerializer
+
+
+class LogOutApiView(APIView):
+
+    @staticmethod
+    def post( self, request ):
+        # Todo get use name from request , print in response
+        response = Response("Logged Out Successfully!")
+        response.set_cookie("refresh_token", "", expires = 0)
+        response.set_cookie("access_token", "", expires = 0)
+        return response
 
 
 class AccountViewSet(viewsets.ViewSet):
