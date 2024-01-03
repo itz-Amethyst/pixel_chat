2069f116203dae5db4b28d261f3cebca53797bf5
itz-amethyst <pransermi@gmail.com>
2024-01-03 21:42:54 +0330

    feat: create user api

--- PixelChat/account/serializers.py
+++ PixelChat/account/serializers.py
@@ -7,3 +7,25 @@ class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ("username",)
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Account
+        fields = ("username", "password",)
+
+    # To check If user exists
+    def is_valid( self, raise_exception = False):
+        valid = super().is_valid(raise_exception = raise_exception)
+
+        if valid:
+            username = self.validated_data["username"]
+            if Account.objects.exists(username = username):
+                self._errors["username"] = [f"Username: {username} exists."]
+                valid = False
+
+        return valid
+
+    def create(self, validated_data):
+        user = Account.objects.create(**validated_data)
+        return user
--- PixelChat/account/views.py
+++ PixelChat/account/views.py
@@ -1,11 +1,33 @@
-from rest_framework import viewsets
+from rest_framework import viewsets , status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Account
 from .schemas import user_list_docs
-from .serializers import AccountSerializer
+from .serializers import AccountSerializer , RegisterSerializer
+
+
+class RegisterUserView(APIView):
+    def post( self, request ):
+        serializer = RegisterSerializer(data = request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data["username"]
+
+            # Prevent these create action by these names
+            forbidden_name = ["admin", "root", "superuser", "milad"]
+            if username is forbidden_name:
+                return Response({"error:": f"Username {username} not allowed!"}, status = status.HTTP_409_CONFLICT)
+
+            serializer.save()
+            return Response({"success": f"User {username} successfully created"}, status = status.HTTP_200_OK)
+
+        errors = serializer.errors
+        if "username" in errors and "non_field_errors" not in errors:
+            return Response({"error:": errors["username"][0]})
+
+        return Response(errors, status = status.HTTP_400_BAD_REQUEST)
+
 
 
 class LogOutApiView(APIView):
