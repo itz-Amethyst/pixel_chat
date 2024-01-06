2866354f536012d925c673496a149131843b8d9b
itz-amethyst <pransermi@gmail.com>
2024-01-06 22:50:21 +0330

    feat: Added server membership url

--- PixelChat/server/urls.py
+++ PixelChat/server/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import ServerListViewSet, CategoryListViewSet
+from .views import ServerListViewSet, CategoryListViewSet, ServerMembershipViewSet
 
 router = DefaultRouter()
 router.register(r'select', ServerListViewSet)
 router.register(r'category', CategoryListViewSet)
+router.register(r'membership/(?P<server_id>\d+)/membership', ServerMembershipViewSet, basename = "server-membership")
